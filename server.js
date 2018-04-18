872b3d2a53e32675cb87917055339629b84d931d
Pasindu Perera <pasindubawantha@gmail.com>
2018-04-18 20:55:38 +0530

    hosting_porposes_only

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const environment = require('./environment')
 const PORT = process.env.PORT || 5000
 const app = express()
 const bodyParser = require('body-parser')
+const request = require('request');
 
 firebase.initializeApp(environment.firebase);
 
@@ -11,6 +12,115 @@ const db = firebase.database();
 
 var usersRef = db.ref("users/");
 var homeRef = db.ref("home/");
+var notificationRef =db.ref('/home/notifaction_on');
+
+
+var sendMessageOn = 1;
+
+notificationRef.on("value", function(n) {
+  sendMessageOn = n.val()
+}, function (errorObject) {
+  console.log("error reading notification status" + errorObject.code);
+});
+
+
+function handleMessage(sender_psid, received_message) {
+
+  	let response;
+  	homeRef.update({fb_puid:sender_psid})
+  	if(sendMessageOn == 1){
+  		response = {
+		  "attachment": {
+		    "type": "template",
+		    "payload": {
+			  "template_type":"button",
+			  "text":"Notifications is turned on. do u want to turn it off",
+			  "buttons":[
+		          {
+		            "type": "postback",
+		            "title": "Turn off",
+		            "payload": "off",
+		          }
+			  ]
+			}
+		  }
+		}
+  	} else {
+  		response = {
+		  "attachment": {
+		    "type": "template",
+		    "payload": {
+			  "template_type":"button",
+			  "text":"Notifications is turned off. do u want to turn it on",
+			  "buttons":[
+		          {
+		            "type": "postback",
+		            "title": "Turn on",
+		            "payload": "on",
+		          }
+			  ]
+			}
+		  }
+		}
+
+  	}
+	
+  
+  // Sends the response message
+  callSendAPI(sender_psid, response);    
+}
+
+function handlePostback(sender_psid, received_postback) {
+  let response;
+  
+  // Get the payload for the postback
+  let payload = received_postback.payload;
+
+  // Set the response based on the postback payload
+  if (payload === 'on') {
+  	sendMessageOn = 1 
+  	notificationRef.set(1)
+    response = { "text": "Notoficaions are On" }
+  } else if (payload === 'off') {
+  	sendMessageOn = 0
+  	notificationRef.set(0)
+    response = { "text": "Notoficaions are Off" }
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
+}
+
+function callSendAPI(sender_psid, response) {
+	const PAGE_ACCESS_TOKEN = environment.fb_messnger.page_access_token;
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
+}
+
+function sendAlltert(sender_psid,alert_object) {
+	if(sender_psid){
+		callSendAPI(sender_psid,JSON.stringify(alert_object))
+	}
+	
+}
 
 function measure(lat1, lon1, lat2, lon2){  // generally used geo measurement function
     var R = 6378.137; // Radius of earth in KM
@@ -24,9 +134,6 @@ function measure(lat1, lon1, lat2, lon2){  // generally used geo measurement fun
     return d * 1000; // meters
 }
 
-function sendAlltert(alert_object) {
-
-}
 
 homeRef.on("value", function(home) {
 	var home = home.val()
@@ -60,7 +167,7 @@ homeRef.on("value", function(home) {
 	  					var distance = measure(x1,y1,x2,y2)
 	  					if(distance <= home.alert_radius){
 	  						var alert_object = JSON.parse(JSON.stringify(users[home.monitor_users[j]].data[i]))
-	  						sendAlltert(alert_object)
+	  						sendAlltert(home.fb_puid, alert_object)
 	  					}
 	  				}
 	  				
@@ -77,7 +184,7 @@ homeRef.on("value", function(home) {
 });
 
 
-var state = "nothing"
+
 app.use(bodyParser.json())
 
 app.get('/fb_messager', function(req, res) {
@@ -107,6 +214,18 @@ app.post('/webhook', (req, res) => {
       // will only ever contain one event, so we get index 0
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+
+      // Get the sender PSID
+	  let sender_psid = webhook_event.sender.id;
+	  console.log('Sender PSID: ' + sender_psid);
+
+	  // Check if the event is a message or postback and
+	  // pass the event to the appropriate handler function
+	  if (webhook_event.message) {
+	    handleMessage(sender_psid, webhook_event.message);        
+	  } else if (webhook_event.postback) {
+	    handlePostback(sender_psid, webhook_event.postback);
+	  }
       
     });
 
@@ -124,7 +243,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
   
   /** UPDATE YOUR VERIFY TOKEN **/
-  const VERIFY_TOKEN = environment.fb_messnger.page_access_token;;
+  const VERIFY_TOKEN = environment.fb_messnger.my_verify_token
   
   // Parse params from the webhook verification request
   let mode = req.query['hub.mode'];
@@ -147,6 +266,11 @@ app.get('/webhook', (req, res) => {
     }
   }
 });
+
+app.get('/fb', (req, res)=> {
+	res.status(200).send('<a gref="http://m.me/<PAGE_NAME"> Go to link and send message to start notifications </a>')
+
+})
 
 
 app.listen(PORT, () => console.log('Example app listening on port '.concat('',PORT)))
